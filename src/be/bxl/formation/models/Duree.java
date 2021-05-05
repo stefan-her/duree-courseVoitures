251b467233d41a395ad59690c8fc124222fb714b
Stefan Her <stefan.her@gmail.com>
2021-05-05 00:41:51 -0700

    un seul parametre

--- src/be/bxl/formation/models/Duree.java
+++ src/be/bxl/formation/models/Duree.java
@@ -4,26 +4,21 @@ import java.util.HashMap;
 
 public class Duree {
 
-    private int totSecondsOrg;
-    private int totSecondsMod;
+    private int totSec;
 
     public Duree(int sec) {
-        if(sec < 0) {
-            throw new IllegalArgumentException("Pas de nombre négartif");
-        }
-
-        this.totSecondsOrg = sec;
-        this.totSecondsMod = this.totSecondsOrg;
+        this.totSec = sec;
+        this.setDuree(sec);
     }
 
     public Duree(int jour, int heure, int minute, int seconde) {
         if(jour < 0 || heure < 0 || heure > 24 || minute < 0 || minute > 60|| seconde < 0 || seconde > 60) {
-            throw new IllegalArgumentException("Pas de nombre négartif");
+            throw new IllegalArgumentException("respecter la valeur des temps");
         }
 
         int sec = (jour * 86400) + (heure * 3600) + (minute * 60) + seconde;
-        this.totSecondsOrg = sec;
-        this.totSecondsMod = this.totSecondsOrg;
+        this.totSec = sec;
+        this.setDuree(sec);
     }
 
     private HashMap convertSecondToHumain(int sec) {
@@ -36,27 +31,31 @@ public class Duree {
     }
 
     public String getDuree() {
-        HashMap dhms = this.convertSecondToHumain(this.totSecondsMod);
+        HashMap dhms = this.convertSecondToHumain(this.totSec);
         String textPattern = " ->  %s jours %s heures %s minutes %s secondes";
         String text = String.format(textPattern, dhms.get("days"), dhms.get("hours"), dhms.get("min"), dhms.get("sec"));
         return text;
     }
 
+    private void setDuree(int sec) {
+        if(sec < 0) {
+            throw new IllegalArgumentException("pas de temps négatif");
+        }
+        this.totSec = sec;
+    }
+
     public int getTotalSeconde() {
-        return this.totSecondsMod;
+        return this.totSec;
     }
 
     //Prend un objet de type Duree en paramètre et soustrait cette dernière à notre objet.
     public void subDuree(Duree obj) {
-        int tmp = this.totSecondsOrg - obj.totSecondsOrg;
-        if(tmp < 0) {
-            throw new IllegalArgumentException("pas de temps négatif");
-        }
-        this.totSecondsMod = tmp;
+        int tmp = this.totSec - obj.totSec;
+        this.setDuree(tmp);
     }
 
     //Même principe, mais la Duree reçue est ajoutée à notre objet.
     public void addDuree(Duree obj) {
-        this.totSecondsMod = this.totSecondsOrg + obj.totSecondsOrg;
+        this.setDuree(this.totSec + obj.totSec);
     }
 }
